3ba51579b8bf94b8eff8c9725aa195674960e30e
Shivansh11956 <157223986+Shivansh11956@users.noreply.github.com>
2025-08-07 14:13:42 +0530

    new

--- app.py
+++ app.py
@@ -51,5 +51,5 @@ def index():
     return render_template("index.html", stocks=breakouts)
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # fallback to 5000 for local
+    port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
